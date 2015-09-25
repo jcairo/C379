f6609f95c45ecd1012796b200876dea1fc107f38
jcairo <joncairo@gmail.com>
2015-09-25 09:47:09 -0600

    Updates to process parser.

--- process_manager.c
+++ process_manager.c
@@ -1,11 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "process_manager.h"
 
 #define MAX_CHARS 1000
 
+/* Checks whether a passed in process name is running. Passes back pids. */
+int get_pids_by_name(char *process_name) {
+    FILE *input;
+    char buffer[MAX_CHARS];
+
+    // Construct the required command.
+    char command_prefix[] = "ps aux | grep ";
+    char command_postfix[] = " | grep -v grep | awk '{print $2, $NF;}'";
+
+    // Concat the command components.
+    char command[512];
+    strcat(command, command_prefix);
+    strcat(command, process_name);
+    strcat(command, command_postfix);
+
+    
+    // Run the command.
+    printf("%s", command);
+    // input = popen(command)
+
+}
+
+
+
 /* Checks whether and existing procnanny process is already running */
 int proc_nanny_running() {
+    get_pids_by_name("procnanny");
+    FILE *input;
+    char buffer[MAX_CHARS];
+    input = popen("ps aux | grep procnanny", "r");
+    if (input == NULL) {
+        printf("Error when running 'ps aux grep procnanny'"); 
+    }
     return 1;
 
     int is_running = 1;
